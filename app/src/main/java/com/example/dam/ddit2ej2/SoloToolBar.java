cd82d63722ee075b60451a737ac5b7ab51387ba8
aida566 <aidagutierrezgutierrez@gmail.com>
2018-10-14 21:34:07 +0200

    SoloToolbar terminada

--- app/src/main/java/com/example/dam/ddit2ej2/SoloToolBar.java
+++ app/src/main/java/com/example/dam/ddit2ej2/SoloToolBar.java
@@ -1,13 +1,79 @@
 package com.example.dam.ddit2ej2;
 
+import android.content.Intent;
+import android.os.Build;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SoloToolBar extends AppCompatActivity {
+
+    Toolbar tb;
+    ActionBar ab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solo_tool_bar);
+
+        tb = (Toolbar) findViewById(R.id.tb);
+
+        setSupportActionBar(tb);
+
+        ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+            case R.id.itOpcion1:
+
+                return true;
+
+            case R.id.itOpcion2:
+
+                return true;
+
+            case R.id.itOpcion3:
+
+                return true;
+
+            case R.id.itOpcion4:
+
+                return true;
+
+            case R.id.itOpcion5:
+
+                return true;
+
+            case R.id.itOpcion6:
+
+                return true;
+
+            case android.R.id.home:
+
+                Intent i = new Intent(SoloToolBar.this, Principal.class);
+                startActivity(i);
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
